84d1330c9283aac721143068c2eaa136e96ab409
Athena Hernández <64385980+athenachernandez@users.noreply.github.com>
2020-06-23 19:21:18 -0700

    done!

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,38 @@
 package _02_gotta_catchem_all;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class ExceptionMethods {
+	//2. In the ExceptionMethods class, write a method called divide that takes
+		//   two doubles as parameters and returns their quotient as a double. This method 
+		//   should throw an IllegalArgumentException if the denominator is 0.0.
+	public double divide(double x, double y) {
+		if (y == 0.0) {
+			throw new IllegalArgumentException();
+		} else {
+			double quotient = x / y;
+			return quotient;
+		}
+		
+
+	}
+	
+	// 4. In the ExceptionMethods class, write a method called reverseString that
+		// takes a
+		// String and returns the reverse of that String. It should throw an
+		// IllegalStateException
+		// if the String passed in is empty
+	
+	public String reverseString(String str) {
+		String reverseStr = "";
+		if (!str.isEmpty()) {
+			for (int i = str.length()-1; i >=0; i--) {
+				reverseStr += str.charAt(i);
+			}
+			return reverseStr;
+		} else {
+			throw new IllegalStateException();
+		}	
+	}
 	
 }
--- src/_02_gotta_catchem_all/ExceptionsTest.java
+++ src/_02_gotta_catchem_all/ExceptionsTest.java
@@ -4,48 +4,58 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-
 class ExceptionsTest {
 	ExceptionMethods em = new ExceptionMethods();
-	
-	//1. This is an example of how one might test that an exception is thrown.
-	//   It tests that an IndexOutOfBoundsException is thrown 
-	@Test
-	public void testIndexOOBException() {
-		int[] vars = {0, 1, 2, 3};
-			
-		try {
-			vars[0] = 4;
-			assertArrayEquals(vars, new int[] {4, 1, 2, 3});
-			vars[4] = 5;
-			fail("IndexOutOfBoundsException not thrown");
-		}catch(IndexOutOfBoundsException e) {
-			e.printStackTrace();
-			assertArrayEquals(vars, new int[] {4, 1, 2, 3});
-		}
-	}
-	
-	//2. In the ExceptionMethods class, write a method called divide that takes
-	//   two doubles as parameters and returns their quotient as a double. This method 
-	//   should throw an IllegalArgumentException if the denominator is 0.0.
-	
-	//3. Complete the JUnit test method to test the divide method.
+
+	// 1. This is an example of how one might test that an exception is thrown.
+//	// It tests that an IndexOutOfBoundsException is thrown
+//	@Test
+//	public void testIndexOOBException() {
+//		int[] vars = { 0, 1, 2, 3 };
+//
+//		try {
+//			vars[0] = 4;
+//			assertArrayEquals(vars, new int[] { 4, 1, 2, 3 });
+//			vars[4] = 5;
+//			fail("IndexOutOfBoundsException not thrown");
+//		} catch (IndexOutOfBoundsException e) {
+//			e.printStackTrace();
+//			assertArrayEquals(vars, new int[] { 4, 1, 2, 3 });
+//		}
+//	}
+
+	// 2. In the ExceptionMethods class, write a method called divide that takes
+	// two doubles as parameters and returns their quotient as a double. This method
+	// should throw an IllegalArgumentException if the denominator is 0.0.
+
+	// 3. Complete the JUnit test method to test the divide method.
 	@Test
 	public void testDivideByZeroException() {
-		
+
+		try {
+			assertEquals(4.0, em.divide(8.0, 2.0));
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			assertEquals(4.0, em.divide(8.0, 2.0));
+		}
+
 	}
-	
-	//4. In the ExceptionMethods class, write a method called reverseString that takes a
-	//   String and returns the reverse of that String. It should throw an IllegalStateException
-	//   if the String passed in is empty
-	
-	//5. Complete the JUnit test method to test the reverseStringMethod.
+
+	// 4. In the ExceptionMethods class, write a method called reverseString that
+	// takes a
+	// String and returns the reverse of that String. It should throw an
+	// IllegalStateException
+	// if the String passed in is empty
+
+	// 5. Complete the JUnit test method to test the reverseStringMethod.
 	@Test
 	public void testReverseString() {
-		
+		try {
+			assertEquals("athena", em.reverseString("anehta"));
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+			assertEquals("athena", em.reverseString("anehta"));
+		}
 	}
-	
-	
-	
 
 }
